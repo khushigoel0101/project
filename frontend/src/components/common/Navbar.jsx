bb6a4d96fc58d2fb3abd8f8b4d2ddb03f3427968
khushigoel0101 <kgoel286@gmail.com>
2025-06-15 12:58:13 +0530

    second

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { HiOutlineUser, HiOutlineShoppingBag, HiBars3BottomRight} from 'react-icons/hi2'
 import SearchBar from './SearchBar'
+import CartDrawer from '../Layout/CartDrawer'
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
+    <>
     <nav className="container mx-auto flex items-center justify-between py-4 px-6">
       {/* Logo */}
       <div>
@@ -32,17 +40,26 @@ const Navbar = () => {
         <Link to="/profile" className="hover:text-black">
           <HiOutlineUser className='h-6 w-6 text-gray-700'/>
         </Link>
-        <button className="relative hover:text-black">
+        <button onClick = {toggleCartDrawer} className="relative hover:text-black">
         <HiOutlineShoppingBag className='h-6 w-6 text-gray-700' />
-        <span className="absolute top-1 right-0 bg-red-500 text-white text-xs rounded-full px-2" />
+        <span className="absolute -top-1 bg-red-500 text-white text-xs rounded-full px-2 py-0.5" >
+        4
+        </span>
         </button>
         {/*search */}
-        <SearchBar />
+        <div className='overflow-hidden'>
+          <SearchBar />
+        </div>
+        
         <button className='md:hidden'>
           <HiBars3BottomRight className='h-6 w-6 text-gray-700' />
         </button>
       </div>
     </nav>
+    <CartDrawer drawerOpen={drawerOpen} toggleCartDrawer={toggleCartDrawer}>
+
+    </CartDrawer>
+    </>
   )
 }
 
