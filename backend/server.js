3b0f4f03cccc31d4a0b6ed3ff0dea85cad742014
khushigoel0101 <kgoel286@gmail.com>
2025-08-11 22:38:20 +0530

    reset

---
+++ backend/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/ProductRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const checkoutRoutes = require('./routes/checkoutRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+const subscribeRoutes = require('./routes/subscriberRoutes');
+const adminRoutes = require("./routes/adminRoutes")
+const productAdminRoutes = require("./routes/productAdminRoutes")
+const adminOrderRoutes= require("./routes/adminOrderRoutes")
+
+dotenv.config();  // Load env first
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectDB();
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to API');
+});
+
+// API Routes
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/checkout', checkoutRoutes)
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+app.use('/api', subscribeRoutes);
+
+//admin routes
+app.use("/api/admin/users", adminRoutes)
+app.use("/api/admin/products", productAdminRoutes)
+app.use("/api/admin/orders", adminOrderRoutes)
+
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
